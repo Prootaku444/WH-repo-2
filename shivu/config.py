eddaf36800a78ce9a14b6e860327e26be39c114f
Prootaku444 <prootaku782@gmail.com>
2024-12-09 23:48:12 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "5909658683"
     sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083", "8035449599"
     GROUP_ID = -1002311769574
-    TOKEN = "6950971614:AAHIo1vorcKFfaw8PCkITBGfM070Z2jKCCA"
+    TOKEN = "7655351916:AAG5p2KxTldZzXIvrZJ5F8TOmHC8XuexjP4"
     mongo_url = "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/wy70cl.jpg", "https://files.catbox.moe/wy70cl.jpg"]
     SUPPORT_CHAT = "WH_SUPPORT_GC"
